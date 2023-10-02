4585901350d3d2073940257f429cf9e730f31d0a
aa-r-ju <aarju3447@gmail.com>
2023-10-02 16:48:47 +0545

    feat:completed part13 c video3

--- notes-server/controllers/users.js
+++ notes-server/controllers/users.js
@@ -1,14 +1,23 @@
 const router = require("express").Router();
 
-const { User, Note } = require("../models");
+const { User, Note, Team } = require("../models");
 const { tokenExtractor, isAdmin } = require("../util/middleware");
 
 router.get("/", async (req, res) => {
   const users = await User.findAll({
-    include: {
-      model: Note,
-      attributes: { exclude: ["userId"] },
-    },
+    include: [
+      {
+        model: Note,
+        attributes: { exclude: ["userId"] },
+      },
+      {
+        model: Team,
+        attributes: ["name", "id"],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
   });
   res.json(users);
 });
@@ -23,9 +32,20 @@ router.post("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const user = await User.findByPk(req.params.id);
+  const user = await User.findByPk(req.params.id, {
+    include: {
+      model: Note,
+    },
+  });
   if (user) {
-    res.json(user);
+    user.notes.forEach((note) => {
+      console.log(note.content);
+    });
+    res.json({
+      username: user.username,
+      name: user.name,
+      note_count: user.notes.length,
+    });
   } else {
     res.status(404).end();
   }
